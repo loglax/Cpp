effafc82b86c81b2da2be27767997e49ac315ac4
loglax <kvllogesh4010@gmail.com>
2025-03-11 12:08:57 +0530

    file added

---
+++ 2_library.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int days;
+    cout<<"Enter the number of days you are late : ";
+    cin>>days;
+    if(days > 0 && days <=5)
+    {
+        cout <<"\nPer day Your Fine amount is : 0.50 Rupees";
+        cout<<"\nTotal Fine amount is : "<<days * 0.50 << " Rupees";
+    }
+    else if(days >= 6 && days <= 10)
+        {
+            cout<<"\nPer day Your Fine amount is : 1.00 Rupees";
+            cout<<"\nTotal Fine amount is : "<<days * 1.00<<" Rupees";
+        }
+    
+        else if(days > 10 && days <= 30)
+            {
+                cout<<"\nPer day Your Fine amount is : 5 Rupees";
+                cout<<"\nTotal Fine amount is : "<<days * 5<<" Rupees";
+            }
+            else
+                {
+                    cout<<"\nYour Membership is Cancelled";
+                }
+}
