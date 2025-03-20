1d8f4f77d6fedf1480044b13cff6a8948a1f2333
loglax <kvllogesh4010@gmail.com>
2025-03-20 10:34:59 +0530

    prime number added

---
+++ 10_prime_number.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n, c=0;
+    cout<<"Enter a number : ";
+    cin>>n;
+        for(int i=1;i<=n;i++)
+        {
+            if(n%i==0)
+            c++;
+        }
+        if(c==2)
+        {
+            cout<<n<< " is a prime number"<<endl;
+        }
+        else
+        {
+            cout<<n<<" is not a prime number"<<endl;
+        }
+    return 0;
+}
